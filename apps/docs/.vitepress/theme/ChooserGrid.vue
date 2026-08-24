3fc8a8ea2e7e099730ef2e36a484f6a969d77d48
Emilola Divine <emiloladivine123@gmail.com>
2026-08-24 14:12:26 +0100

    feat: add Fastify and NestJS icons to chooser grid

--- apps/docs/.vitepress/theme/ChooserGrid.vue
+++ apps/docs/.vitepress/theme/ChooserGrid.vue
@@ -22,26 +22,10 @@ defineProps({
       >
          <span class="chooser-icon" aria-hidden="true">
             <img
-               v-if="['express', 'prisma', 'typeorm', 'mikroorm'].includes(item.icon)"
-               :src="withBase(`/icons/${item.icon}.svg`)"
+               v-if="['express', 'fastify', 'nest', 'prisma', 'typeorm', 'mikroorm'].includes(item.icon)"
+               :src="withBase(`/icons/${item.icon === 'nest' ? 'nestjs' : item.icon}.svg`)"
                alt=""
             />
-            <svg v-else-if="item.icon === 'fastify'" viewBox="0 0 48 48" fill="none">
-               <path
-                  d="M26 6 10 28h12l-4 14 20-26H26l4-10Z"
-                  stroke="currentColor"
-                  stroke-width="2"
-                  stroke-linejoin="round"
-               />
-            </svg>
-            <svg v-else-if="item.icon === 'nest'" viewBox="0 0 48 48" fill="none">
-               <path
-                  d="M24 8c8 6 12 12 12 20a12 12 0 0 1-24 0c0-8 4-14 12-20Z"
-                  stroke="currentColor"
-                  stroke-width="2"
-               />
-               <path d="M24 16c4 4 6 8 6 12a6 6 0 1 1-12 0c0-4 2-8 6-12Z" stroke="currentColor" stroke-width="2" />
-            </svg>
             <svg v-else-if="item.icon === 'drizzle'" viewBox="0 0 48 48" fill="none">
                <path
                   d="M18 8c0 8-8 12-8 20a8 8 0 0 0 16 0c0-8-8-12-8-20Z"
